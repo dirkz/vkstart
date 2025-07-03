1a382d82ad408e24c6bacc4ad5527f3003d27b31
Dirk Zimmermann <me@dirkz.com>
2025-07-03 16:10:31 +0200

    Initial (VS created)

---
+++ vulkan-hpp-sdl.cpp
@@ -0,0 +1,12 @@
+﻿// vulkan-hpp-sdl.cpp : Defines the entry point for the application.
+//
+
+#include "vulkan-hpp-sdl.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
