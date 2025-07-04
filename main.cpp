9820c90db47c26824336902233050fcfeef47a78
Dirk Zimmermann <me@dirkz.com>
2025-07-04 17:35:15 +0200

    Use SDL_MAIN_USE_CALLBACKS at the right place

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
-﻿#define SDL_MAIN_USE_CALLBACKS 1 /* use the callbacks instead of main() */
-#include <SDL3/SDL.h>
+﻿#include <SDL3/SDL.h>
+
+#define SDL_MAIN_USE_CALLBACKS 1 /* use the callbacks instead of main() */
 #include <SDL3/SDL_main.h>
+
 #include <SDL3/SDL_vulkan.h>
 
 #define VULKAN_HPP_ENABLE_DYNAMIC_LOADER_TOOL 0
