cac34242ca5462eaf1440a9ccad3c19b23b434e4
Dirk Zimmermann <me@dirkz.com>
2025-07-23 13:22:16 +0200

    UpdateUniformBuffer

--- vkstart/Engine.cpp
+++ vkstart/Engine.cpp
@@ -788,6 +788,24 @@ void Engine::CreateSyncObjects()
 
 void Engine::UpdateUniformBuffer(uint32_t currentImage)
 {
+    static auto startTime = std::chrono::high_resolution_clock::now();
+
+    auto currentTime = std::chrono::high_resolution_clock::now();
+    float time =
+        std::chrono::duration<float, std::chrono::seconds::period>(currentTime - startTime).count();
+
+    UniformBufferObject ubo{};
+    ubo.model =
+        glm::rotate(glm::mat4(1.0f), time * glm::radians(90.0f), glm::vec3(0.0f, 0.0f, 1.0f));
+    ubo.view = lookAt(glm::vec3(2.0f, 2.0f, 2.0f), glm::vec3(0.0f, 0.0f, 0.0f),
+                      glm::vec3(0.0f, 0.0f, 1.0f));
+    ubo.proj = glm::perspective(glm::radians(45.0f),
+                                static_cast<float>(m_swapchainExtent.width) /
+                                    static_cast<float>(m_swapchainExtent.height),
+                                0.1f, 10.0f);
+    ubo.proj[1][1] *= -1; // !!! correct for vulkan's inverted y-axis !!!
+
+    memcpy(m_uniformBuffersMapped[currentImage], &ubo, sizeof(ubo));
 }
 
 } // namespace vkstart
